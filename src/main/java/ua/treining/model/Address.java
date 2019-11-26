1299a8aeb414dce2fd00447d2ea03d9c2e3db3b5
= <=>
2019-11-26 20:57:00 +0200

    Initial commit

---
+++ src/main/java/ua/treining/model/Address.java
@@ -0,0 +1,64 @@
+package ua.treining.model;
+
+/**
+ * Created by Olena Orel on 26.11.2019.
+ */
+public class Address {
+
+    private String index;
+    private String cityName;
+    private String street;
+    private int houseNumber;
+    private int apartmentNumber;
+
+    public Address() {
+    }
+
+    public Address(String index, String cityName, String street, int houseNumber, int apartmentNumber) {
+        this.index = index;
+        this.cityName = cityName;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public int getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(int apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+}
---
+++ src/main/java/ua/treining/model/Model.java
@@ -0,0 +1,7 @@
+package ua.treining.model;
+
+/**
+ * Created by Olena Orel on 26.11.2019.
+ */
+public class Model {
+}
