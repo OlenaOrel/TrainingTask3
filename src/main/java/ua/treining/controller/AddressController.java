88df273806f009915ef9ecf0c4eaba3ed2c3e04a
= <=>
2019-11-29 15:39:26 +0200

    created AddressController logic

---
+++ src/main/java/ua/treining/controller/AddressController.java
@@ -0,0 +1,54 @@
+package ua.treining.controller;
+
+import ua.treining.model.Model;
+import ua.treining.model.UserAddress;
+import ua.treining.view.ViewMessages;
+
+public class AddressController {
+
+    private UtilityController contrUtil;
+    private Model model;
+
+    public AddressController(UtilityController contrUtil, Model model) {
+        this.contrUtil = contrUtil;
+        this.model = model;
+    }
+
+    UserAddress setInputUserAddress() {
+        UserAddress address = model.getUserAddress();
+        address.setPostalCode(getCorrectInputPostalCode());
+        address.setCityName(getCorrectInputCityName());
+        address.setStreet(getCorrectInputStreet());
+        address.setHouseNumber(getCorrectInputHouseNumber());
+        address.setApartmentNumber(getCorrectInputApartmentNumber());
+        return address;
+    }
+
+    private String getCorrectInputPostalCode() {
+        return contrUtil.inputStringValueWithScanner(ViewMessages.INPUT_POSTAL_CODE,
+                RegexController.POSTAL_CODE);
+    }
+
+    private String getCorrectInputCityName() {
+        return contrUtil.inputStringValueWithScanner(ViewMessages.INPUT_CITY_NAME,
+                RegexController.CITY_NAME);
+    }
+
+    private String getCorrectInputStreet() {
+        return contrUtil.inputStringValueWithScanner(ViewMessages.INPUT_STREET,
+                RegexController.STREET_NAME);
+    }
+
+    private int getCorrectInputHouseNumber() {
+        String res = contrUtil.inputStringValueWithScanner(ViewMessages.INPUT_HOUSE_NUMBER,
+                RegexController.NUMBER_OF_HOUSE_OR_APARTMENT);
+        return Integer.parseInt(res);
+    }
+
+    private int getCorrectInputApartmentNumber() {
+        String res = contrUtil.inputStringValueWithScanner(ViewMessages.INPUT_APARTMENT_NUMBER,
+                RegexController.NUMBER_OF_HOUSE_OR_APARTMENT);
+        return Integer.parseInt(res);
+    }
+
+}
